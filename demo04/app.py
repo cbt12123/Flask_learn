02e3bb9cc12860890266dbbd241c4309c04eaa14
Noahcbt <1318053457@qq.com>
2024-04-24 17:45:13 +0800

    push zxwl 2 git, finish 90% sql.

--- demo04/app.py
+++ demo04/app.py
@@ -28,9 +28,82 @@ db = SQLAlchemy(app)
 #         rs = conn.execute(text("select 1"))
 #         print(rs.fetchone())
 
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # varchar, null=0
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    user_articles = db.relationship("Article", back_populates="author")
+
+# user = User(username="法外狂徒", password='123654')
+# sql: insert user(username, password) values('法外狂徒', '123654');
+
+class Article(db.Model):
+    __tablename__ = "article"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    # 添加作者的外键
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    # backref: 会自动给User模型添加一个articles的属性，用来获取文章列表
+    author = db.relationship("User", back_populates="user_articles")
+
+article = Article(title="Flask学习大纲", content="Flaskxxxx")
+# articel.author_id = user.id
+# user = User.query.get(articel.author_id)
+# articel.author = User.query.get(articel.author_id)
+# print(articel.author)
+# sqlalchemy/flask
+
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/user/add')
+def add_user():
+    # 1. 创建ORM对象
+    user = User(username="法外狂徒", password='123654')
+    # 2. 将ORM对象添加到db.session中
+    db.session.add(user)
+    # 3. 将db.session中的改变同步到数据中
+    db.session.commit()
+    return "用户创建成功"
+
+@app.route('/user/query')
+def query_user():
+    # 1. get查找
+    # user = User.query.get(1)
+    # print(f'{user.id}: {user.username}-{user.password}')
+    # 2. filter_by查找
+    # Query: 类数组
+    users = User.query.filter_by(username="法外狂徒")
+    for user in users:
+        print(user.username)
+    return "数据查找成功"
+
+@app.route('/user/update')
+def update_user():
+    user = User.query.filter_by(username="法外狂徒").first()
+    user.password = "23232303"
+    db.session.commit()
+    return "数据修改成功"
+
+@app.route('/user/delete')
+def delete_user():
+    # 1. 查找
+    user = User.query.get(1)
+    # 2. 删除
+    db.session.delete(user)
+    # 3. 将db.session中的修改同步到数据库中
+    db.session.commit()
+    return "数据删除成功"
 
 if __name__ == '__main__':
     app.run()
