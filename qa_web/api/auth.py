4e8c7a41008593afa0337a1b57af02decb0dc464
Noahcbt <1318053457@qq.com>
2024-04-29 18:05:26 +0800

    push zxwl 2 git

---
+++ qa_web/api/auth.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+# /auth
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+@bp.route("/login")
+def login():
+    pass
+
+@bp.route("register")
+def register():
+    return render_template("register.html")
--- qa_web/app.py
+++ qa_web/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 import config
 from exts import db
 from models import UserModule
+from api.qa import bp as qa_bp
+from api.auth import bp as auth_bp
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 # 绑定配置文件
@@ -9,9 +12,14 @@ app.config.from_object(config)
 
 db.init_app(app)
 
-@app.route('/')
-def hello_world():
-    return 'hello world'
+migrate = Migrate(app, db)
+
+app.register_blueprint(qa_bp)
+app.register_blueprint(auth_bp)
+
+# blueprint：用来做模块化
+# 电影，读书，音乐，xxx
+
 
 if __name__ == '__main__':
     app.run()
